49696be884767ef7f806f3c63e7b926937ca7ac3
shah-chayan9 <chayan.shah@cogoport.com>
2024-09-29 08:33:06 -0700

    First Commit

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <p>How to reach you.</p>
+  </section>
+);
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>Projects</h2>
+    <p>Details of your projects.</p>
+  </section>
+);
+
+export default Projects;
